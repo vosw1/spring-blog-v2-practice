ab1aab44fdcc980f9c174bd99f610c769e2e6f9b
vosw1 <vosw1@naver.com>
2024-03-15 15:25:42 +0900

    builder 패턴 이용해서 user_tb 만들기

---
+++ src/main/java/shop/mtcoding/blog/user/User.java
@@ -0,0 +1,38 @@
+package shop.mtcoding.blog.user;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Data
+@Table(name = "user_tb")
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+    private String email;
+
+    @CreationTimestamp
+    private Timestamp created_at;
+
+    @Builder
+    public User(Integer id, String username, String password, String email, Timestamp created_at) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.created_at = created_at;
+    }
+}
---
+++ src/main/java/shop/mtcoding/blog/user/UserRepository.java
@@ -0,0 +1,18 @@
+package shop.mtcoding.blog.user;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Repository
+public class UserRepository {
+    private final EntityManager em;
+
+    @Transactional
+    public User save(User user){
+        em.persist(user);
+        return user;
+    }
+}
